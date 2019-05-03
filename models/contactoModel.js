d0985ca2c3641883ebf2e283dbc9138cec09c758
SpaceBear96 <cafu.aguilar@tecsup.edu.pe>
2019-05-03 04:06:27 -0500

    Mejora

--- models/contactoModel.js
+++ models/contactoModel.js
@@ -1,1 +1,13 @@
-contactoModel
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let modelSchema = new Schema({
+	nombre : { type: String, required: true },
+	apellido : String,
+	email: { type: String, required: true },
+	date: Date,
+	perfil: { type: String, required: true}
+});
+
+let model = mongoose.model('contacts',modelSchema,'contacts');
+module.exports = model;
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,51 @@ var upload = multer({storage: storage});
 //-----------------------
 var AWS = require('aws-sdk');
 var stream = require('stream');
-	
-router.get('/', function (req, res) {
-    res.render('index');
+
+let model = require('../models/contactoModel');
+
+router.get('/ingresar', function (req, res) {
+	res.render('index');
 });
- 
+
+router.get('/', function (req, res) {
+    model.find({}).exec(function(err,data){
+			if(err){
+				console.log(err);
+				res.sendStatus(500);
+			}else{
+				res.render('listar',{lista:data});
+			}
+		});
+});
+
+router.get('/filtro', function (req, res) {
+    model.find({"email" : "leidy@gmail.com"}).exec(function(err,data){
+			if(data){
+				res.render('listar',{lista:data});
+			}else{
+				res.redirect('/');
+			}
+		});
+});
+
+router.get('/delete/:id', function (req, res) {
+	var id = req.params.id;
+    model.deleteOne({"_id" : id}).exec(function(err,data){
+    		res.redirect('/');
+		});
+});
 router.post('/cargar', upload.single('file'),function(req,res){
-	
+
+	let obj = new model;
+	var d = new Date(req.body.date);
+	obj.nombre = req.body.nombre;	
+	obj.apellido= req.body.apellido;
+	obj.email = req.body.email;
+	obj.date = d.toDateString();
+
+
+
 	var s3Client = new AWS.S3({
     	accessKeyId: 'AKIASWIVSPOOAUMEJ42F',
    		secretAccessKey: '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',
@@ -25,10 +63,12 @@ router.post('/cargar', upload.single('file'),function(req,res){
          Bucket: 'jabappdemo', 
          Key: '', // pass key
          Body: null, // pass file body
+         ACL: 'public-read-write'
 	};
 	
 	var name = Date.now()+"_"+req.file.originalname;
 
+	obj.perfil= name;
 	uploadParams.Key = name;
 	uploadParams.Body = req.file.buffer;
 		
@@ -36,7 +76,15 @@ router.post('/cargar', upload.single('file'),function(req,res){
 		if (err) {
 			res.status(500).json({error:"Error -> " + err});
 		}
-		res.json({message: 'File uploaded successfully! -> keyname = ' + req.file.originalname});
+	});
+	
+	obj.save(function(err,newData){
+			if(err){
+				console.log(err);
+				res.sendStatus(500);
+			}else{
+				res.redirect('/');
+			}
 	});
 });
 
